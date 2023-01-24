143f0ff98f8b16da95f6a3f108cf4dbc81fbdeb2
Cristian132dev <cristian132dev@gmail.com>
2023-01-23 21:46:47 -0500

    modificando el DOM

---
+++ Manipulando_DOM/escribiendo_html_desde_js/script.js
@@ -0,0 +1,38 @@
+/* metodos que selecciona etiquetas del html y elementos de css (para estos se usa el estandar de css) */
+const h1 = document.querySelector("h1")
+const p = document.querySelector("p")
+const parrafito = document.querySelector(".parrafito")
+const pid = document.querySelector("#pid")
+const input = document.querySelector("input")
+
+/* agrupa los elementos a mostrar en la consola en un objeto */
+console.log({
+   h1,
+   p,
+   parrafito,
+   pid,
+   input
+});
+
+console.log(h1); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
