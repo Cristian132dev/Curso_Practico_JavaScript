5052f19ec565386afca7f69aa12bb291134f8583
Cristian132dev <cristian132dev@gmail.com>
2023-01-24 22:54:10 -0500

    modificaciones al DOM

--- Manipulando_DOM/escribiendo_html_desde_js/script.js
+++ Manipulando_DOM/escribiendo_html_desde_js/script.js
@@ -14,14 +14,19 @@ console.log({
    input
 });
 
-console.log(h1); 
+console.log(h1);
+
+/* innerHTML cambia el documento html y acepta etiquetas  */
+h1.innerHTML = "<i>cambio con</i> <br>Javascript";
+
+/*  innerText modifica unicamente el texto, hace el codigo mas seguro ante ataques */
+parrafito.innerText = "nombre una cosa rebarbara"
 
 
+/*  la forma habitual de hacer este tipo de cambios es con getAtribute y setAtribute, que nos devuelve o modifica los atributos de nuestros elementos */
 
-
-
-
-
+console.log(h1.getAttribute("class"));
+h1.setAttribute("class", "rojo")
 
 
 
